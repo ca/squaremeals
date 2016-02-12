518670c91e3d3bc8353ffdd80e4247fc8c6d7777
Alex Vallorosi <alexvallorosi@gmail.com>
2016-02-12 13:18:30 -0500

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send({'API Version': '1.0'});
+});
+
+router.get('/intake', function(req, res, next) {
+
+  var TDEE = req.query.tdee;
+
+  var activityLevel = req.query.activityLevel;
+  var weight = req.query.weight;
+  var goal = req.query.goal;
+  var multiplier, kilocalories, protein, fat, carbs;
+
+  if (activityLevel < 5) { multiplier = 1.5; } 
+  if (activityLevel > 2) { multiplier = 1.3; }
+  if (activityLevel >= 5) { multiplier = 1.8; }
+  
+
+  console.log(req.query);
+
+  kilocalories = weight * 10 * multiplier; // determines MAINTENANCE Calories
+  if (goal == "cut") { kilocalories -= 500; }
+  if (goal == "bulk") { kilocalories += 500; }
+
+  protein = 1.3 * weight;
+  fat = (kilocalories * .2) / 9;
+  carbs = (kilocalories - (protein * 4) - (fat * 9)) / 4;
+
+
+  res.send({
+    'kilocalories': kilocalories,
+    'protein': protein,
+    'carbs': carbs,
+    'fat': fat
+  });
+});
+
+module.exports = router;
