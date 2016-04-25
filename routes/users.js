1459871958a122d310edf11123863c9be43daeb1
Alex Vallorosi <alexvallorosi@gmail.com>
2016-04-25 00:10:21 -0400

    check if user account exists, otherwise save it in the DB

--- routes/users.js
+++ routes/users.js
@@ -11,16 +11,33 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
-  var username = req.query.username,
-      email = req.query.email;
+  // var username = req.query.username,
+  var email = req.query.email;
 
-  db.get().collection('users').insertOne({
-    'username': username,
+  db.get().collection('users').findOne({
     'email': email
   }, function(err, result) {
-      console.log("Inserted a document into the users collection.");
-      res.send("COMPLETED");
+    if (result == null) {
+      db.get().collection('users').insertOne({
+        'email': email
+      }, function(err, r) {
+          console.log("Inserted a document into the users collection.");
+          console.log(r);
+
+          // Erroring here for some reason...
+          res.send("SIGN UP PAGE HERE");
+
+      });
+    }
+    console.log("Found a user, skip to meals page");
+    console.log(result);
+    res.send("MEALS PAGE HERE");
   });
+
 });
+
+// router.get('/find', function(req, res, next) {
+//   var email = req.query.email;
+// });
 
 module.exports = router;
